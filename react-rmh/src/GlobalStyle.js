e091beb90086f0831d01c7ea75f023f3e8bd053e
senukronin <senukronin@gmail.com>
2023-08-15 21:05:05 +0530

    do

--- react-rmh/src/GlobalStyle.js
+++ react-rmh/src/GlobalStyle.js
@@ -7,7 +7,36 @@ export const GlobalStyle = createGlobalStyle`
         --lightGrey: #eee;
         --medGrey: #353535;
         --darkGrey: #1c1c1c;
-        --fontSuper
+        --fontSuperBig: 2.5rem;
+        --fontBig: 1.5rem;
+        --fontMed: 1.2rem;
+        --fontSmall: 1rem;
+    }
+
+    *{
+        box-sizing: border-Box;
+        font-family: 'Abel', sans-serif;
+    }
+
+    body{
+        margin: 0;
+        padding: 0;
+
+        h1{
+            font-size: 2rem;
+            font-weight: 600;
+            color: var(--white);
+        }
+
+        h3 {
+            font-size: 1.1rem;
+            font-weight: 600;
+        }
+
+        p {
+            font-size: 1rem;
+            color: var(--white);
+        }
     }
 
 `;
