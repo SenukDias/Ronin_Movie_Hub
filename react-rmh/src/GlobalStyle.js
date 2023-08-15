c1c8a5f0d4440917d635cae87db1e7cff2ee8022
senukronin <senukronin@gmail.com>
2023-08-15 20:40:40 +0530

    GS.js

--- react-rmh/src/GlobalStyle.js
+++ react-rmh/src/GlobalStyle.js
@@ -1,6 +1,13 @@
 import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-    
+    :root {
+        --maxWidth: 1280px;
+        --white: #fff;
+        --lightGrey: #eee;
+        --medGrey: #353535;
+        --darkGrey: #1c1c1c;
+        --fontSuper
+    }
 
 `;
