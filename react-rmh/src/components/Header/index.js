4f6af440fd38e93c6cb72a8b62e7262c90c0d029
senukronin <senukronin@gmail.com>
2023-08-20 08:16:58 +0530

    Routinng to Movie Id

--- react-rmh/src/components/Header/index.js
+++ react-rmh/src/components/Header/index.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import RMDBLogo from '../../images/react-movie-logo.svg';
-import TMDBLogo from '../../images/tmdb_logo.svg';
+import RMDBLogo from "../../images/react-movie-logo.svg";
+import TMDBLogo from "../../images/tmdb_logo.svg";
 
-import {Wrapper, Content, LogoImg, TMDBLogoImg} from './Header.styles';
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 
 const Header = () => (
-    <Wrapper>
-        <Content>
-            <LogoImg src={RMDBLogo} alt='rmdb-logo' />
-            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo'/>
-        </Content>
-    </Wrapper>
+  <Wrapper>
+    <Content>
+      <Link to="/">
+        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+      </Link>
+
+      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+    </Content>
+  </Wrapper>
 );
 
 export default Header;
