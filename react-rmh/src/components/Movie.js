c31270105ab766c118620d40a79765a11ba9a20d
senukronin <senukronin@gmail.com>
2023-08-20 12:34:29 +0530

    Actor Grid

--- react-rmh/src/components/Movie.js
+++ react-rmh/src/components/Movie.js
@@ -10,6 +10,7 @@ import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 // Hooks
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -34,6 +35,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header="Actors">
+        {movie.actors.map(actor => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
