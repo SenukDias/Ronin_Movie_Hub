253333435c21fdd326fad73752765d67d1256538
senukronin <senukronin@gmail.com>
2023-08-20 10:53:21 +0530

    Main Movie Page

--- react-rmh/src/components/Movie.js
+++ react-rmh/src/components/Movie.js
@@ -1,5 +1,41 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const  Movie = () => <div>Movie</div>
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
+// Components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
+
+// Hooks
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
+// Image
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  const { movieId } = useParams();
+
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went Wrong ....!</div>;
+
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+    </>
+  );
+};
 
 export default Movie;
